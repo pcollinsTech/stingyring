5097b82d4acffb4b3b504ab8591ba616a0341341
pcollinsTech <phil@pcollins.tech>
2020-08-04 10:31:56 +0100

    Tracking key

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-174473343-1`,
       },
     },
     `gatsby-plugin-feed`,
